3c89108563f2dc74a3197c25271504cc37ad0c17
theoboldalex <theoboldalex@gmail.com>
2021-02-20 14:57:46 +0000

    basic schema and persist

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
 const { sequelize, dbConn } = require("./config/db.js");
 const colors = require("colors");
+const Record = require("./models/Record.js");
 
 // Connect to DB
 dbConn();
+
+// Create Records table on server start -> TESTING ONLY
+Record.sync({ force: true })
+	.then((done) => {
+		if (done) console.log("Table created");
+	})
+	.catch((err) => console.error(err));
