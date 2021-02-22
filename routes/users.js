b4df7556bed2f01300307f0539fd7626af7dda27
theoboldalex <theoboldalex@gmail.com>
2021-02-22 15:44:57 +0000

    basic input validation on register

--- routes/users.js
+++ routes/users.js
@@ -3,33 +3,48 @@ const router = express.Router();
 const ServiceResponse = require("../ServiceResponse");
 const User = require("../models/User");
 const bcrypt = require("bcryptjs");
+const { body, validationResult } = require("express-validator");
 
-router.post("/register", async (req, res) => {
-  try {
-    const { username, email, password } = req.body;
-    const serviceResponse = new ServiceResponse();
+// @route POST /register
+// @desc Register a user
+// @access Public
+router.post(
+  "/register",
+  [body("email").isEmail(), body("password").isLength({ min: 6 })],
+  async (req, res) => {
+    try {
+      const { username, email, password } = req.body;
+      const serviceResponse = new ServiceResponse();
+      const errors = validationResult(req);
 
-    const hashedPassword = await bcrypt.hash(password, 10);
+      if (!errors.isEmpty()) {
+        serviceResponse.success = false;
+        serviceResponse.message = errors.array();
+        res.status(400).json(serviceResponse);
+      }
 
-    const user = await User.create({
-      username,
-      email,
-      password: hashedPassword,
-    });
+      const hashedPassword = await bcrypt.hash(password, 10);
 
-    if (!user) {
-      serviceResponse.success = false;
-      serviceResponse.message = "User could not be created.";
-      res.status(400).json(serviceResponse);
+      const user = await User.create({
+        username,
+        email,
+        password: hashedPassword,
+      });
+
+      if (!user) {
+        serviceResponse.success = false;
+        serviceResponse.message = "User could not be created.";
+        res.status(400).json(serviceResponse);
+      }
+
+      serviceResponse.data = user;
+
+      res.status(201).json(serviceResponse);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server error.");
     }
-
-    serviceResponse.data = user;
-
-    res.status(201).json(serviceResponse);
-  } catch (err) {
-    console.error(err.message);
-    res.status(500).send("Server error.");
   }
-});
+);
 
 module.exports = router;
